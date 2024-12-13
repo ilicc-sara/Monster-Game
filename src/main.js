b03b00636984ed913c981fa979594089819a7481
Sara <sarailic431@gmail.com>
2024-12-13 19:29:44 +0100

    adding functions

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 "use strict";
 import "./style.css";
+
+const form = document.querySelector(".form");
+
+const monsterContainer = document.querySelector(".monster-container");
+
+const monsterCreator = function () {
+  const monster = { id: crypto.randomUUID(), isDone: false };
+
+  const getMonster = () => monster;
+
+  const createMonster = () => `<div class="monster-box">MONSTER</div>`;
+  return { getMonster, createMonster };
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const creatorMonster = monsterCreator();
+  console.log(creatorMonster.getMonster());
+
+  const newMonster = document.createElement("div");
+  newMonster.classList.add("monster-box");
+  newMonster.innerHTML = `MONSTER`;
+
+  console.log(newMonster);
+
+  monsterContainer.appendChild(newMonster);
+});
